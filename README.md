57428af34b3f7ae8398f4ee5d6857f302f61498d
Mike Gauthier <gauthiermichael3.14@gmail.com>
2019-08-31 22:58:28 -0700

    README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Acid Dip Test Machine
+Repo of python for Raspberry Pi driven Acid Dip Test Machine for QC
+Mike Gauthier
--- run.py
+++ run.py
@@ -7,6 +7,11 @@ from datetime import datetime
 class AcidDipTester():
   
   def __init__(self):
+    self.linactpin = 23
+    self.sonicpin = 25
+    self.pwrsplypin = 12
+    self.sparepin = 16
+    self.pins = [23,25,12,16]
     lcd = lcddriver.lcd()
     lcd.lcd_clear()
     time.sleep(1)
@@ -14,5 +19,9 @@ class AcidDipTester():
     lcd.lcd_display_string("    Booting....",3)
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
+    for x in self.pins:
+      GPIO.setup(x, GPIO.OUT)
+      GPIO.output(x, GPIO.HIGH)
 
-if __name__ = "
+if __name__=="__main__":
+    acid = AcidDipTester()
