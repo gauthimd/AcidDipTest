ae875d283574fb9d6601facca8d902c0dfa15269
Mike Gauthier <gauthiermichael3.14@gmail.com>
2019-09-03 23:58:03 -0700

    basic menu working

--- rotarylcd.py
+++ rotarylcd.py
@@ -11,10 +11,65 @@ GPIO.setmode(GPIO.BCM)
 lcd = lcddriver.lcd()
 rot = KY040.KY040(21,20,24)
 
-lcd.lcd_clear()
-lcd.lcd_display_string("Hello",1)
-lcd.lcd_display_string("Hello",2)
-lcd.lcd_display_string("Hello",3)
-lcd.lcd_display_string("Hello",4)
-time.sleep(4)
-lcd.lcd_clear()
+class menu():
+
+    def __init__(self):
+        self.stringdict = {1:"",2:"",3:"",4:""}
+        self.blinkline = 1
+        self.blinkon = 0
+        self.button = 0
+        self.lcd = lcddriver.lcd()
+        self.rot = KY040.KY040(21,20,24,self.rotarycallback,self.switchcallback)
+        self.rot.start()
+
+    def hello(self):
+        for x in self.stringdict:
+            self.stringdict[x] = "Hello"
+        self.lcd.lcd_clear()
+        y = 1
+        z = 0
+        while True:
+            if self.blinkon == 1:
+                for x in self.stringdict:
+                    if x == self.blinkline:
+                        if z == 1:
+                            self.lcd.lcd_display_string(self.stringdict[self.blinkline],self.blinkline)
+                            z = 0
+                        else: 
+                            self.lcd.lcd_display_string("",self.blinkline)
+                            z = 1
+                    else:
+                        self.lcd.lcd_display_string(self.stringdict[x],x)
+                time.sleep(.5)
+                self.lcd.lcd_clear()
+            else: 
+                for x in self.stringdict:
+                    self.lcd.lcd_display_string(self.stringdict[x],x)
+                time.sleep(.5)
+        self.lcd.lcd_clear()
+
+    def rotarycallback(self,direction):
+        if self.blinkon == 1:
+            if direction == 0: 
+                self.blinkline +=1
+                if self.blinkline > 4: self.blinkline = 1
+            else:
+                self.blinkline -=1
+                if self.blinkline < 1: self.blinkline = 4
+        print(self.blinkline)
+
+    def switchcallback(self):
+        print("Button pressed")
+        if self.blinkon == 0: self.blinkon = 1
+        else: self.blinkon = 0
+
+    def run(self):
+        try:
+            self.hello()
+        except KeyboardInterrupt:
+            self.rot.stop()
+            GPIO.cleanup()
+
+if __name__=="__main__":
+    menu = menu()
+    menu.run()
